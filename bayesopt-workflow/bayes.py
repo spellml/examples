dd089a986f758eeeb512763f5968baf011a6bf97
Nikhil Bhatia 2 <nikhil@ip-10-2-183-45.us-west-2.compute.internal>
2019-01-28 20:51:59 -0500

    all fixes

--- bayesopt-workflow/bayes.py
+++ bayesopt-workflow/bayes.py
@@ -31,38 +31,36 @@ parser.add_argument('--metric', type=str,
 parser.add_argument('--parallel-runs', type=int,
                     dest='parallel', help='number of runs that will be spawned in parallel',
                     default=3)
-parser.add_argument('--iterations', type=int,
-                    dest='iterations', help='number of times new parallel runs will be spawned',
-                    default=3)
+parser.add_argument('--max-runs', type=int,
+                    dest='maxruns', help='number of times new parallel runs will be spawned',
+                    default=9)
 parser.add_argument('--optimize', type=str,
-                    dest='optimize', help='maximize or minimize', default="maximize")
+                    dest='optimize', help='maximize or minimize', default="maximize", choices=['maximize', 'minimize'])
 parser.add_argument('--machine-type', type=str,
-                    dest='machine', help='machine type', default="K80")
+                    dest='machine', help='machine type', default="K80", choices=['K80', 'CPU'])
 parser.add_argument('--metric-type', type=str,
-                    dest='type', help='How to evaluate metric (average, last, max)', default="average")
+                    dest='type', help='How to evaluate metric (average, last, max)', default="average", choices=['average', 'last', 'max'])
 args = parser.parse_args()
 
 
-class SER:
+class ParallelRun:
     """
-    Represents the suggest-evaluate-register bayesian optimization
-    paradigm corresponding to a last suggested param (last_param)
-    and a corresponding SER output (last_output). Maintains invariant that
-    an instance of SER will not "suggest" a new point before having
-    registered a new evaluated datapoint.
+    Represents a parallel run with a last evaluated param (last_param),
+    and a corresponding output (last_output). Ensures that only this instance
+    of a ParallelRun will be able to register the (param, output) pair it evaluated.
+    Uses a lock to handle the optimizer (shared between all ParallelRuns),
+    maintains invariant that one ParallelRun will not "suggest" a new point before
+    having registered a new evaluated datapoint.
     """
+
     def __init__(self):
         self.last_param = None
         self.last_output = None
 
     def step(self, optimizer, f):
-        # need to lock so that no other thread can interleave and register a point
-        # before the current thread finishes 1. registering its last point and 2. calling
-        # suggest to get its next point
         lock.acquire()
         try:
             if self.last_output is not None:
-                # print('registering {0} --> {1}'.format(self.last_param, self.last_output))
                 optimizer.register(self.last_param, self.last_output)
             self.last_param = optimizer.suggest(utility)
         finally:
@@ -77,7 +75,7 @@ def black_box_function(**pvals):
     black_box_function required by Bayesian Optimizer
 
     @param pvals, a flexible length dictionary mapping hyperparameters
-    to chosen values
+    to chosen values (ex: {'batch-size': 32, 'dropout': .1})
     @throws Exception if metric to track is not found in metrics for the run
     """
 
@@ -99,40 +97,7 @@ def black_box_function(**pvals):
 
     print("\t[{0}] run spawned: $ {1}".format(r.id, command))
 
-    # get metric value
-    metrics = []
-    for m in r.metrics(metric_name=args.metric, follow=True):
-        metrics.append(m[2])
-    if not metrics:
-        raise Exception('Run ended prematurely or metric ({0}) was not found (check run logs)'.format(args.metric))
-
-    if args.type == 'average':
-        metric_value = reduce(lambda x, y: x + y, metrics) / len(metrics)
-    elif args.type == 'last':
-        metric_value = metrics[-1]
-    elif args.type == 'max':
-        metric_value = max(metrics)
-    else:
-        #TODO validate at args parsing level
-        raise Exception('Invalid metric type ({0})'.format(args.type))
-
-    r.wait_status(client.runs.COMPLETE)
-
-    print("\t[{0}] run output: {1} ({2})".format(r.id, round(metric_value, 2), args.metric))
-
-    return metric_value if args.optimize == 'maximize' else (-1)*metric_value
-
-def f2(x):
-    """
-    Testing function
-    """
-    x[0] = x[0]*15
-    x[1] = (x[1]*15)-5
-
-    y = np.square(x[1] - (5.1/(4*np.square(math.pi)))*np.square(x[0]) + (5/math.pi)*x[0] - 6) + 10*(1-(1./(8*math.pi)))*np.cos(x[0]) + 10;
-
-    result = y
-    return result
+    return get_metric_value(r)
 
 def parse_params():
     """
@@ -142,39 +107,65 @@ def parse_params():
     param_to_min_max = {k: (v[0], v[1]) for k, v in tup.items()}
     return param_to_min_max
 
-#set up optimizer
-pbounds = parse_params()
-# print('pbounds are -> {0}'.format(pbounds))
-optimizer = BayesianOptimization(
-    f=black_box_function,
-    pbounds=pbounds,
-    verbose=2,
-    random_state=1
-)
+def get_metric_value(r):
+    metrics = [m[2] for m in r.metrics(metric_name=args.metric, follow=True)]
 
-#set up args.parallel SER runs
-parallel_runs = []
-for i in range(args.parallel):
-    parallel_runs.append(SER())
+    if not metrics:
+        raise Exception('Run ended prematurely or metric ({0}) was not found (check run logs)'.format(args.metric))
 
-for i in range(args.iterations):
-    print('start (iteration {0})'.format(i))
+    if args.type == 'average':
+        metric_value = sum(metrics) / float(len(metrics))
+    elif args.type == 'last':
+        metric_value = metrics[-1]
+    elif args.type == 'max':
+        metric_value = max(metrics)
+    else:
+        raise Exception('Invalid metric type ({0})'.format(args.type))
 
-    parallel_threads = list(map(lambda run: Thread(target=run.step, args=(optimizer, black_box_function)), parallel_runs))
+    r.wait_status(client.runs.COMPLETE)
 
-    for thread in parallel_threads:
-        thread.start()
+    print("\t[{0}] run output: {1} ({2})".format(r.id, round(metric_value, 2), args.metric))
 
-    for thread in parallel_threads:
-        thread.join()
+    return metric_value if args.optimize == 'maximize' else (-1)*metric_value
 
-    print('end (iteration {0})\n'.format(i))
+def main():
+    #set up optimizer
+    pbounds = parse_params()
+    # print('pbounds are -> {0}'.format(pbounds))
+    optimizer = BayesianOptimization(
+        f=black_box_function,
+        pbounds=pbounds,
+        verbose=2,
+        random_state=1
+    )
 
-for run in parallel_runs:
-    run.finish(optimizer)
+    #set up args.parallel ParallelRuns
+    parallel_runs = []
+    for i in range(args.parallel):
+        parallel_runs.append(ParallelRun())
 
-print("LOCAL MAX:")
-print('\t' + str(optimizer.max))
+    for i in range(int(args.maxruns/args.parallel)):
+        print('start (iteration {0})'.format(i))
 
-# for i, res in enumerate(optimizer.res):
-#     print("Iteration {}: \n\t{}".format(i, res))
+        parallel_threads = list(map(lambda run: Thread(target=run.step, args=(optimizer, black_box_function)), parallel_runs))
+
+        for thread in parallel_threads:
+            thread.start()
+
+        for thread in parallel_threads:
+            thread.join()
+
+        print('end (iteration {0})\n'.format(i))
+
+    for run in parallel_runs:
+        run.finish(optimizer)
+
+    print("LOCAL MAX:")
+    print('\t' + str(optimizer.max))
+
+    # for i, res in enumerate(optimizer.res):
+    #     print("Iteration {}: \n\t{}".format(i, res))
+
+
+if __name__ == '__main__':
+    main()
