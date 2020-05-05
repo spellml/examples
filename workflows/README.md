9431449f76f710316456e968679aa111ea9e3a14
Aleksey <aleksey@spell.run>
2020-05-05 11:51:32 -0400

    Commit.

---
+++ workflows/README.md
@@ -0,0 +1,16 @@
+# workflows
+
+This folder contains a tutorial and some example scripts covering [Spell workflows](http://aleksey.spell:3000/docs/workflow_overview/).
+
+## Tutorial
+
+![](https://i.imgur.com/W5Ugs0S.png)
+
+Check out the `workflows-demo.ipynb` notebook to get started. You can run this tutorial on Spell using:
+
+```bash
+spell jupyter \
+    --lab \
+    --github-url https://github.com/spellrun/spell-examples.git \
+    workflows-demo-workspace
+```
---
+++ workflows/workflow.py
@@ -0,0 +1,50 @@
+import spell.client
+client = spell.client.from_environment()
+
+# create the first run to download the dataset (War and Peace, by Leo Tolstoy)
+# if desired, replace data_url with url to another plain text file to train on
+data_url = "https://www.gutenberg.org/files/2600/2600-0.txt"
+r = client.runs.new(
+    command="wget -O input.txt {}".format(data_url)
+)
+print("waiting for run {} to complete".format(r.id))
+r.wait_status(*client.runs.FINAL)
+r.refresh()
+if r.status != client.runs.COMPLETE:
+    raise OSError(f"failed at run {r.id}")
+
+# create the second run to train char-RNN on the dataset
+data_dir = "/data"
+r = client.runs.new(
+    machine_type="V100",
+    command="python train.py --data_dir={}".format(data_dir),
+    attached_resources={
+        "runs/{}/input.txt".format(r.id): "{}/input.txt".format(data_dir)
+    },
+    commit_label="char-rnn",
+)
+print("waiting for run {} to complete".format(r.id))
+r.wait_status(*client.runs.FINAL)
+r.refresh()
+if r.status != client.runs.COMPLETE:
+    raise OSError(f"failed at run {r.id}")
+
+# create the third run that samples the model to generate some text
+r = client.runs.new(
+    machine_type="V100",
+    command="python sample.py",
+    attached_resources={"runs/{}/save".format(r.id): "save"},
+    commit_label="char-rnn",
+)
+print("waiting for run {} to complete".format(r.id))
+r.wait_status(*client.runs.FINAL)
+r.refresh()
+if r.status != client.runs.COMPLETE:
+    raise OSError(f"failed at run {r.id}")
+
+# print the logs from the last run
+# generated text should be the last log line
+print("Logs from run {}:".format(r.id))
+for line in r.logs():
+    if line.status == client.runs.RUNNING and not line.status_event:
+        print(line)
