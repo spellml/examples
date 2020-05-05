ee49b7914934c6ea83bf3fba41d7e4f17526bccb
Aleksey <aleksey@spell.run>
2020-05-05 18:27:53 -0400

    Reorg.

--- workflows/workflow.py
+++ workflows/workflow.py
@@ -4,47 +4,42 @@ client = spell.client.from_environment()
 # create the first run to download the dataset (War and Peace, by Leo Tolstoy)
 # if desired, replace data_url with url to another plain text file to train on
 data_url = "https://www.gutenberg.org/files/2600/2600-0.txt"
-r = client.runs.new(
+r1 = client.runs.new(
     command="wget -O input.txt {}".format(data_url)
 )
-print("waiting for run {} to complete".format(r.id))
-r.wait_status(*client.runs.FINAL)
-r.refresh()
-if r.status != client.runs.COMPLETE:
-    raise OSError(f"failed at run {r.id}")
+print("waiting for run {} to complete".format(r1.id))
+r1.wait_status(*client.runs.FINAL)
+r1.refresh()
+if r1.status != client.runs.COMPLETE:
+    raise OSError(f"failed at run {r1.id}")
 
 # create the second run to train char-RNN on the dataset
 data_dir = "/data"
-r = client.runs.new(
+r2 = client.runs.new(
     machine_type="V100",
     command="python train.py --data_dir={}".format(data_dir),
     attached_resources={
-        "runs/{}/input.txt".format(r.id): "{}/input.txt".format(data_dir)
+        "runs/{}/input.txt".format(r1.id): "{}/input.txt".format(data_dir)
     },
     commit_label="char-rnn",
 )
-print("waiting for run {} to complete".format(r.id))
-r.wait_status(*client.runs.FINAL)
-r.refresh()
-if r.status != client.runs.COMPLETE:
-    raise OSError(f"failed at run {r.id}")
+print("waiting for run {} to complete".format(r2.id))
+
+r2.wait_status(*client.runs.FINAL)
+r2.refresh()
+if r2.status != client.runs.COMPLETE:
+    raise OSError(f"failed at run {r2.id}")
 
 # create the third run that samples the model to generate some text
-r = client.runs.new(
+r3 = client.runs.new(
     machine_type="V100",
     command="python sample.py",
-    attached_resources={"runs/{}/save".format(r.id): "save"},
+    attached_resources={"runs/{}/save".format(r3.id): "save"},
     commit_label="char-rnn",
 )
-print("waiting for run {} to complete".format(r.id))
-r.wait_status(*client.runs.FINAL)
-r.refresh()
-if r.status != client.runs.COMPLETE:
-    raise OSError(f"failed at run {r.id}")
+print("waiting for run {} to complete".format(r3.id))
 
-# print the logs from the last run
-# generated text should be the last log line
-print("Logs from run {}:".format(r.id))
-for line in r.logs():
-    if line.status == client.runs.RUNNING and not line.status_event:
-        print(line)
+r3.wait_status(*client.runs.FINAL)
+r3.refresh()
+if r3.status != client.runs.COMPLETE:
+    raise OSError(f"failed at run {r3.id}")
