c72044c046be7b1eb40893b8eaff2e66fd8a11a4
ianvonseggern1 <ian@spell.run>
2020-09-04 17:29:16 -0400

    generalize

--- modelservers/cifar/query_server.py
+++ modelservers/cifar/query_server.py
@@ -2,16 +2,12 @@ import base64
 import requests
 import time
 
-URL = "http://spell-org.spell-org.spell.services/spell-org/cifar/predict"
-#"http://ian-local-aws.ian-local-aws.local.spell.services/ian-local-aws/cifar/predict"
-#"http://dev-aws-east-1.spell-external.dev.spell.services/spell-external/cifar10/predict"
+URL = "YOUR SERVERS PREDICT ENDPOINT"
+IMAGE_FILE_PATH = "dog1.jpeg"
 
-with open("dog1.jpeg", "rb") as f:
+with open(IMAGE_FILE_PATH, "rb") as f:
     image_content = base64.b64encode(f.read()).decode('utf-8')
 
-while True:
-    start = time.time()
-    response = requests.post(URL, json={ "img": image_content })
-    print("Answer: {} ({:.0f} ms)".format(response.text, (time.time() - start) * 1000))
-    time.sleep(0.001)
-
+start = time.time()
+response = requests.post(URL, json={ "img": image_content })
+print("Answer: {} ({:.0f} ms)".format(response.text, (time.time() - start) * 1000))
