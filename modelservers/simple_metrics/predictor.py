aff179283b7cba9d2961d81fa6da9557ac624163
Caroline Lin <caroline@spell.run>
2020-10-22 13:44:23 -0400

    Format with black

--- modelservers/simple_metrics/predictor.py
+++ modelservers/simple_metrics/predictor.py
@@ -4,18 +4,26 @@ import json
 from spell.serving import BasePredictor
 import spell.serving.metrics as m
 
-# create prometheus metric (y'know, in case you want it)
 
 class Predictor(BasePredictor):
     """ Simple metrics example. """
+
     def __init__(self):
         # prometheus metrics objects can only be defined once
-        self.as_prom_gauge = m.prometheus.Gauge("prometheus_minutes_since_hour", "minutes past the hour")
-        self.with_labels = m.prometheus.Gauge("prometheus_minutes_since_hour_with_labels", "minutes past the hour", ["spell_metric_name", "spell_tag"])
+        self.as_prom_gauge = m.prometheus.Gauge(
+            "prometheus_minutes_since_hour", "minutes past the hour"
+        )
+        self.with_labels = m.prometheus.Gauge(
+            "prometheus_minutes_since_hour_with_labels",
+            "minutes past the hour",
+            ["spell_metric_name", "spell_tag"],
+        )
 
     def predict(self, payload):
         now = datetime.now()
-        minutes_since_hour = (now - now.replace(minute=0, second=0, microsecond=0)).total_seconds() / 60.
+        minutes_since_hour = (
+            now - now.replace(minute=0, second=0, microsecond=0)
+        ).total_seconds() / 60.0
 
         # send a metric
         m.send_metric("minutes_since_hour", minutes_since_hour)
@@ -30,7 +38,9 @@ class Predictor(BasePredictor):
         self.as_prom_gauge.set(minutes_since_hour)
 
         # use the prometheus client library with spell's labels
-        self.with_labels.labels(spell_metric_name="as prometheus metric", spell_tag=str(now.hour))
+        self.with_labels.labels(
+            spell_metric_name="as prometheus metric", spell_tag=str(now.hour)
+        )
 
-        return json.dumps({"response" : minutes_since_hour})
+        return json.dumps({"response": minutes_since_hour})
 
