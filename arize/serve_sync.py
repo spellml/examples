ca89e5641607af55d4c5ff76dd82a1e793d9adb2
Aleksey Bilogur <aleksey@residentmar.io>
2021-02-03 19:25:15 -0500

    Initial commit.

---
+++ arize/serve_sync.py
@@ -0,0 +1,44 @@
+import os
+import uuid
+from asyncio import wrap_future
+
+import numpy as np
+import lightgbm as lgb
+
+from spell.serving import BasePredictor
+from arize.api import Client
+
+class PythonPredictor(BasePredictor):
+    def __init__(self):
+        self.model = lgb.Booster(model_file="/model/churn_model/lgb_classifier.txt")
+        self.arize_client = Client(
+            organization_key=os.environ['ARIZE_ORG_KEY'],
+            api_key=os.environ['ARIZE_API_KEY']
+        )
+        self.model_id = 'churn-model'
+        self.model_version = '0.0.1'
+
+    async def predict(self, request):
+        payload = request['payload']
+        # use np.round to squeeze to binary {0,1}
+        results = list(np.round(self.model.predict(payload)))
+
+        futures = []
+        for result in results:
+            prediction_id = str(uuid.uuid4())
+            future = self.arize_client.log_prediction(
+                model_id=self.model_id,
+                model_version=self.model_version,
+                prediction_id=prediction_id,
+                prediction_label=bool(result)  # {1,0} => {true,false}
+            )
+            future = wrap_future(future)  # SO#34376938
+            futures.append(future)
+        for future in futures:
+            await future
+            status_code = future.result().status_code
+            if status_code != 200:
+                raise IOError(f"Could not reach Arize! Got error code {status_code}.")
+
+        response = {'result': results}
+        return response
